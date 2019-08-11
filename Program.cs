6c0cb9e7f03a84b204680ad121e7677e0ef0b485
Oliver <Oliver.Hemker@gmail.com>
2019-08-11 11:56:41 +0200

    Erste Stufe funktioniert - auch über Pfeiltasten, noch keine Rekursion eingebunden

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace SierpinskiTeppich
+{
+   
+    class Sierpinski : Form
+    {
+        int iteration = 0;
+        public Sierpinski()
+        {
+            Width = 600;
+            Height = 600;
+            Text = "Sierpinski-Teppich"; 
+        }
+        static void Main()
+        {
+            Application.Run(new Sierpinski());
+        }
+        override protected void OnPaint(PaintEventArgs e)
+        {
+            RectangleF bounds = e.Graphics.VisibleClipBounds; // um die Groesse des sichtbaren Bereichs zu ermitteln
+
+            //Brush und Graphics-Object anlegen zur Übergabe an rekursive Methode
+            SolidBrush solidBrush = new SolidBrush(Color.Black);
+            
+
+            //Aufruf der rekusriven Methode
+            // Beginne mit dem Zeichnen, wenn iteration > 0 (Anzahl an Rekursionen > 0)
+
+
+                SierpinskiTeppich(e.Graphics, solidBrush, bounds.X, bounds.Y, bounds.Width, bounds.Height, iteration);
+
+            
+
+        }
+        private void SierpinskiTeppich(Graphics g, Brush brush, Single x, Single y, Single width, Single height, int iteration)
+        {
+            // Rekursion
+            // Zuerst einmal zeichnen, anschließend für die 8 neuen Rechtecke
+            Graphics rectangle = g;
+
+            if(iteration >0)
+            rectangle.FillRectangle(brush,Convert.ToInt16(width / 3.0), Convert.ToInt16(height / 3.0), Convert.ToInt16(width / 3.0), Convert.ToInt16(height/ 3.0));
+
+
+        }
+        override protected void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+            // Mit Pfeiltasten Rekursion verändern
+            // Übergabe an Methode SierpinskiTeppich Paramater iteration
+            if (e.KeyCode == Keys.Up)
+            {
+                //erhöhe Rekursion
+                iteration++;
+                Refresh();
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                // verringere Rekursion 
+                if(iteration > 0)
+                {
+                    iteration--;
+                }
+                Refresh();
+            }
+
+        }
+    }
+}
